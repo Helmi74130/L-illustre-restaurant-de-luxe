da0b8fc13c6ecdebf04d83f0f355f3f863d0a754
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-09-16 11:22:30 +0200

    add l'illustre

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Venez gouter les saveurs exquis de nos plats, notre restaurant se veut être moderne et soigné, une cuisine gastronomique francaise haut de gamme ou chaque bouchée vous offre des saveurs inoubliable.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" integrity="sha384-9+PGKSqjRdkeAU7Eu4nkJU8RFaH8ace8HGXnkiKMP9I9Te0GJ4/km3L1Z8tXigpG" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <title>L'illustre</title>
+  <link rel="shortcut icon" type="image/jpg" href="img/illustre.png"/>
+</head>
+<body id="home">
+  <main>
+    <!-- Navbar -->
+    <nav class=" navbar navbar-expand-lg navbar-dark bg-primary sticky-top col">
+      <div class="container" >
+        <a class="navbar-brand" href="#home"><img width="150px" src="img/illustre1.png" alt="" srcset=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#home">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#menu">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#reservation">Réservez</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Page Home -->
+    <div class="img-bg"></div>
+    <div class="mask"></div>
+    <div class="container">
+      <div class="row">
+        <div class="home-text col-12 d-flex flex-column align-items-center fond">
+          <h1 class="mt-5 reveal-1" >L'ILLUSTRE</h1>
+          <h2 class="reveal-2 mt-2">RESTAURANT</h2>
+          <a class="arrow reveal-3 mt-5" href="#second-page">
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+            </svg></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- Présentation -->
+  <div class="content" id="second-page">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-12 row justify-content-center">
+          <div class="text-center col-12 mt-5">
+            <h3 class="reveal-1" >L'ILLUSTRE</h3>
+            <h4 class="reveal-2">d'une élégance subtile </h4>
+          </div>
+          <div class="mb-5 flower-bg col-12 col-md-10 row justify-content-center mt-5 shadow reveal-3">
+            <div class="mask-flower justify-content-around row text-light text-center reveal-4">
+              <div  class="col-12 col-md-5 mt-5">
+                <h5 class="text-light title-flower text-center">Goutez à la subtilité et à la douceur de nos plats</h5>
+                <p class="mt-5">L'illustre se veut être moderne et soigné, une cuisine gastronomique francaise haut de gamme ou chaque bouchée vous offre des saveurs inoubliable.</p>
+              </div>
+              <div class="col-12 col-md-5 mt-5">
+                <h5 class="text-light title-flower text-center">A votre service depuis plus 40 ans</h5>
+                <p class="mt-5">Géré et menée par le brillant chef francais Richard Dufrez, retrouvez nous à Paris rue la somme à seulement quelques minutes de la tour eiffel.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Menu -->
+    <article class="container-fluid mt-5 mb-5"  id="menu">
+      <h5 class="text-center text-dark title-plat reveal-1">Nos exquis</h5>
+      <div class="row justify-content-center mt-5">
+        <div class="col-12 col-md-12 col-lg-10 row justify-content-around mt-5">
+          <div class="col-12 col-md-5 row reveal-2">
+            <div class="bord col-12 pt-3 pl-3">
+              <img class="col-12 p-0 img-plat" src="img/entree.jpg" alt="" srcset="">
+            </div>
+          </div>
+          <div class="col-12 col-md-5 text-center d-flex flex-column justify-content-center reveal-3">
+            <h5 class="">Nos entrées</h5>
+            <p class="mt-5 reveal-4">Préparez vos pupilles à la dégustation de nos merveilleuses entrées, le tout combinées de plaisir</p>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 col-lg-10 row justify-content-around mt-5 text-center">
+          <div class="col-12 col-md-5 d-flex flex-column justify-content-center reveal-3">
+            <h5 class="">L'art dans votre assiete</h5>
+            <p class="mt-5 reveal-4">Des plats soigneusements préparés et savoureux, aux goûts raffinés. <br> Mélées plaisir et légereté dans un cadre idyllique pour un moment de détente.</p>
+          </div>
+          <div class="col-12 col-md-5 row  justify-content-center">
+            <div class="bord pb-3 pr-3 reveal-2 col-12">
+              <img class="col-12 p-0 img-plat" src="img/plat.jpg" alt="" srcset="">
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 col-lg-10 row justify-content-around mt-5">
+          <div class="col-12 col-md-5 row  justify-content-center">
+            <div class="bord pt-3 pl-3 col-12">
+              <img class="col-12 p-0 img-plat reveal-2" src="img/cake.jpg" alt="" srcset="">
+            </div>
+          </div>
+          <div class="col-12 col-md-5 text-center d-flex flex-column justify-content-center reveal-3">
+            <h5 class="">Nos Désserts</h5>
+            <p class="mt-5 reveal-4">Goutez à nos nombreux désserts aux gouts délicats et parfumés, il ne vous laisserons pas indiférents</p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <!-- Réservation -->
+    <h2 class="text-center reveal-1 mt-5 title-reserv" id="reservation">Réservation</h2>
+    <div class="container-fluid mt-5">
+      <div class="row justify-content-center mt-5 ">
+        <div class="shadow col-lg-10 row reveal-2 fork">
+          <form class=" row p-5 justify-content-center col-12">
+            <h4 class="text-center col-lg-12 mb-5 reveal-3">Réserver une table</h4>
+            <div class="col-md-2 mt-3">
+              <label for="inputCivility">Civilité:</label>
+              <select id="inputCivility" class="form-control">
+                <option selected>Choisir</option>
+                <option>Mme</option>
+                <option>Mr</option>
+              </select>
+            </div>
+            <div class="col-md-5 mt-3">
+              <label for="inputName">Nom</label>
+              <input type="text" class="form-control" id="inputName" placeholder="Nom">
+            </div>
+            <div class="col-md-5 mt-3">
+              <label for="inputFirstname">Prénom</label>
+              <input type="text" class="form-control" id="inputFirstname" placeholder="Prénom">
+            </div>
+            <div class="col-md-6 mt-3">
+              <label for="inputEmail">Email</label>
+              <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+            </div>
+            <div class="col-md-6 mt-3">
+              <label for="inputTime">Date et heure:</label>
+              <input type="datetime-local" class="form-control" id="inputTime">
+            </div>
+            <button type="submit" class="btn btn-primary mt-5">Réserver</button>
+          </form>
+          <div class="col-lg-12 text-center reveal-4">
+            <p class=" text-primary">L'Illustre vous accueillent du mardi au dimanche à partir de 11h30</p>
+            <p>Réservation par téléphone: <a href="tel:+0153741832">01 53 74 18 32</a></p>
+            <p>Réservation par mail: <a href="mailto:reservation@illustre.fr">reservation@illustre.fr</a></p>
+          </div>
+        </div>   
+      </div>
+    </div>
+    <!-- Contact -->
+    <h5 id="contact" class="text-center mt-5 title-reserv reveal-1 col-12">Contact</h5>
+    <div class="container-fluid mt-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-10 row">
+          <img class="col-lg-4 reveal-2" src="img/imgentree.jpg" alt="" srcset="">
+          <div class="col-lg-8">
+            <div class="col-lg-12 row justify-content-center">
+              <form class="col-lg-12 row mt-5 reveal-2" action="">
+                <div class="form-group col-lg-6">
+                  <label for="mailContact" class="form-label mt-4">Email</label>
+                  <input type="email" class="form-control" id="mailContact" placeholder="E-mail">
+                </div>
+                <div class="form-group col-lg-6">
+                  <label for="nameContact" class="form-label mt-4">Nom et prénom</label>
+                  <input type="email" class="form-control" id="nameContact"  placeholder="Nom et Prénom">
+                </div>
+                <div class="form-group col-lg-12">
+                  <label for="messagecontact">Message</label>
+                  <textarea class="form-control" name="" id="messageContact" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mt-5 reveal-3">Envoyer</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Maps -->
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <h6 class="text-center col-12 title-reserv mt-5 reveal-1">Retrouvez-nous</h6>
+        <div class="mapouter mt-5 col-10 reveal-2"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=paris rue de clery&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">Minecraft Website</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
+      </div>
+    </div>
+    <!-- FOOTER -->
+    <footer class="bg-primary text-center text-white mt-5 reveal-3">
+      <div class="container p-4 pb-0">
+        <section class="">
+          <form action="">
+            <div class="row d-flex justify-content-center">
+              <div class="col-auto">
+                <p class="pt-2">
+                  <strong>S'inscrire à notre newsletter</strong>
+                </p>
+              </div>
+              <div class="col-md-5 col-12">
+                <div class="form-outline form-white mb-4">
+                  <input type="email" id="mailsubscribe" class="form-control" />
+                  <label class="form-label" for="mailsubscribe">Adresse e-mail</label>
+                </div>
+              </div>
+              <div class="col-auto">
+                <button type="submit" class="btn btn-outline-light mb-4">
+                  S'inscrire
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+      <div class="text-center p-3" >
+        © 2022 Copyright:
+        <a class="text-white" href="#">L'illustre</a>
+      </div>
+    </footer>
+  </div>
+  <a class="top" href="#home"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+    <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+  </svg></a>
+</body>
+<script src="script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</html>
